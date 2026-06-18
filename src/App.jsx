eaf3c8b67a7e530ad6a5057aa4745afd7c8306e0
Lucie Gantois <luciegantois@gmail.com>
2026-06-18 11:03:24 +0200

    menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import html2canvas from "html2canvas";
 
 import frite from "./assets/corps/frite.png";
 import logoJDL from "./assets/logo/LOGO_JDL.png";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next"
 
 const tete = Object.values(
   import.meta.glob("./assets/chapeaux/*.png", {
@@ -756,6 +756,7 @@ function App() {
           />
         ))}
       </div>
+      <Analytics />
     </div>
   );
 }
